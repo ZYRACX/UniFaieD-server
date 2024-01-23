5e6928170569c47c338360e9a669bcabca4480f3
starzooka <88590630+starzooka@users.noreply.github.com>
2024-01-23 09:25:29 +0000

    prisma initialize with supabase

--- src/app.js
+++ src/app.js
@@ -1,47 +1,37 @@
 const express = require("express")
 const cors = require("cors");
+const {PrismaClient} = require("@prisma/client")
 const app = express()
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+    cors: true
 });
 
+const prisma = new PrismaClient()
 
 const PORT = 8000
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// app.get("/", (req, res) => {
-//     res.send(["hello user"])
-//     // io.on("connect", socket => {
-//     //     // socket.to(to_send).emit("friend-request", {})
-//     //     console.log(socket.id)
-//     // })
-// })
-
-io.on('connection', (socket) => {
+// connect the client
+io.on('connect', (socket) => {
+    // print the socket ID of connected client
     console.log('User connected: ' + socket.id);
-    socket.on("send-request", (data) => {
-        const to_send = data.clientsocketId;
-        const sender_socketID = data.socketID
-        console.log(to_send)
-        socket.to(to_send).emit("friend-request", {requestID:sender_socketID, request:true})
+    socket.on("send-info", (data) => {
+        console.log(data)
     })
+    // disconnect client event
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        // log the ID of of disconnected client
+        console.log('User disconnected: ' + socket.id);
     });
 })
-// app.get("/username/friend_request/to_send", (req, res) => {
-//     console.log(username)
-//     io.on("connect", socket => {
-//         socket.to(to_send).emit("friend-request", {})
-//     })
-// })
+app.post("/auth/register", (req, res) => {
+    const {username, email, password} = req.body
+})
+
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
+
